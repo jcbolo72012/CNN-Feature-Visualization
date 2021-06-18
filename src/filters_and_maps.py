069192ee15d503d5a89f5d8c3c7f9d25253e5df8
John Bolognino <jcbolo72012@gmail.com>
2021-06-18 10:20:05 -0400

    Image + argparse

--- src/filters_and_maps.py
+++ src/filters_and_maps.py
@@ -9,3 +9,20 @@ import torch.nn as nn
 import cv2 as cv
 import argparse
 from torchvision import models, transforms
+
+""" Argument Parsing """
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required=True,
+    help='path to image')
+args = vars(ap.parse_args())
+
+
+
+
+# load the model
+model = models.resnet50(pretrained=True)
+print(model)
+model_weights = [] # we will save the conv layer weights in this list
+conv_layers = [] # we will save the 49 conv layers in this list
+# get all the model children as list
+model_children = list(model.children())
